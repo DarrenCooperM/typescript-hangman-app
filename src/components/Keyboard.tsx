6facc8ea540ce13350be6294a231fede05b462ba
Darren Cooper-Matila <dcoopermatila@gmail.com>
2023-03-01 11:33:39 +1300

    completed hangman app

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import KEYS from "./Letters/Keys";
 import styles from "./Letters/Keyboard.module.css";
 
-export default function Keyboard() {
+type KeyboardProps = {
+  disabled?: boolean;
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+
+export default function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
+}: KeyboardProps) {
   return (
     <div
       style={{
@@ -12,8 +24,17 @@ export default function Keyboard() {
       }}
     >
       {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInActive = inactiveLetters.includes(key);
         return (
-          <button className={`${styles.btn}`} key={key}>
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInActive ? styles.inactive : ""
+            }`}
+            disabled={isInActive || isActive || disabled}
+            key={key}
+          >
             {key}
           </button>
         );
